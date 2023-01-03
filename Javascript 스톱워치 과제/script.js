ff6dbbfc59f244e764dcddc75c381c47b2bdfcfc
abc2767 <73827343+abc2767@users.noreply.github.com>
2023-01-04 02:06:14 +0900

    start 버튼 수정중

--- Javascript 스톱워치 과제/script.js
+++ Javascript 스톱워치 과제/script.js
@@ -1,18 +1,38 @@
+let miliseconds = 0;
+let sec, milisec;
+let active = false;
+const time = document.querySelector(".time");
+const start = document.getElementById("start");
+const stop = document.getElementById("stop");
+const reset = document.getElementById("reset");
+
 function resetClock() {
-  const reset = document.getElementById("reset");
-  const time = document.querySelector(".time");
-  reset.addEventListener("click", () => {
-    time.innerText = "00:00";
-  });
+  active = false;
+  miliseconds = 0;
+  time.innerText = "00:00";
   console.log("reset");
 }
 
-function startClock() {}
-
-function stopClock() {
-  const time = document.querySelector(".time");
+function startClock() {
+  if (active == true) {
+    miliseconds++;
+    time.innerText = printTime();
+  }
 }
 
-function clearRecord() {}
+start.addEventListener("click", () => {
+  active = true;
+  setInterval(startClock, 1000 / 60);
+});
 
-resetClock();
+reset.addEventListener("click", resetClock());
+
+// function stopClock() {}
+
+// function clearRecord() {}
+
+function printTime() {
+  sec = parseInt(miliseconds / 60);
+  milisec = miliseconds % 60;
+  return String(sec).padStart(2, "0") + ":" + String(milisec).padStart(2, "0");
+}
