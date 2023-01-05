7ff6809b427e3aa7e976f73397552b1878abd558
abc2767 <73827343+abc2767@users.noreply.github.com>
2023-01-05 15:11:46 +0900

    1-3

--- python_problem/python_problem_1.py
+++ python_problem/python_problem_1.py
@@ -2,8 +2,15 @@ num = 0
 
 inputNum = input("부를 숫자의 개수를 입력하세요(1, 2, 3만 입력가능) :")
 validInput = [1, 2, 3]
-if not str.isdigit(inputNum):
-  print("정수를 입력하세요")
-elif int(inputNum) not in validInput:
-  print("1,2,3 중 하나를 입력하세요")
-  
+
+while(True):
+  if not str.isdigit(inputNum):
+    print("정수를 입력하세요")
+    inputNum = input("부를 숫자의 개수를 입력하세요(1, 2, 3만 입력가능) :")
+  elif int(inputNum) not in validInput:
+    print("1,2,3 중 하나를 입력하세요")
+    inputNum = input("부를 숫자의 개수를 입력하세요(1, 2, 3만 입력가능) :")
+  else:
+    break
+print(inputNum)
+
