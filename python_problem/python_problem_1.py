aa92a24f0b92fdb792bbaeefff55c71e6b4ca492
abc2767 <73827343+abc2767@users.noreply.github.com>
2023-01-05 16:17:22 +0900

    1-9

--- python_problem/python_problem_1.py
+++ python_problem/python_problem_1.py
@@ -1,28 +1,33 @@
+import random
+
 num = 0
 endGame = False
 validInput = [1, 2, 3]
-playerName = ["playerA", "playerB"]
+playerName = ["computer", "player"]
 
 def brGame(playerNum):
   global num
   global endGame
-  inputNum = input("부를 숫자의 개수를 입력하세요(1, 2, 3만 입력가능) :")
+  
+  if playerNum == 0: #computer case
+    inputNum = random.randint(1,3)
+  else:  #player case
+    inputNum = input("부를 숫자의 개수를 입력하세요(1, 2, 3만 입력가능) :")
 
-  while(True):
-    if not str.isdigit(inputNum):
-      print("정수를 입력하세요")
-      inputNum = input("부를 숫자의 개수를 입력하세요(1, 2, 3만 입력가능) :")
-    elif int(inputNum) not in validInput:
-      print("1,2,3 중 하나를 입력하세요")
-      inputNum = input("부를 숫자의 개수를 입력하세요(1, 2, 3만 입력가능) :")
-    else:
-      inputNum = int(inputNum)
-      break
+    while(True):
+      if not str.isdigit(inputNum):
+        print("정수를 입력하세요")
+        inputNum = input("부를 숫자의 개수를 입력하세요(1, 2, 3만 입력가능) :")
+      elif int(inputNum) not in validInput:
+        print("1,2,3 중 하나를 입력하세요")
+        inputNum = input("부를 숫자의 개수를 입력하세요(1, 2, 3만 입력가능) :")
+      else:
+        inputNum = int(inputNum)
+        break
 
   for i in range(inputNum):
-
     num += 1
-    print(f"{playerName[playerNum]}: {num}")
+    print(f"{playerName[playerNum]}: {num}") 
     if num == 31:
       endGame = True
       break
@@ -34,4 +39,3 @@ while not endGame:
   if endGame:
     break
   brGame(1)
-  
