fb300635ecb41e12b4d971b3680d58db7e4c351b
abc2767 <73827343+abc2767@users.noreply.github.com>
2023-01-05 15:06:57 +0900

    1-3

--- python_problem/python_problem_1.py
+++ python_problem/python_problem_1.py
@@ -1,3 +1,9 @@
 num = 0
 
-inputNum = int(input("부를 숫자의 개수를 입력하세요(1, 2, 3만 입력가능) :"))
+inputNum = input("부를 숫자의 개수를 입력하세요(1, 2, 3만 입력가능) :")
+validInput = [1, 2, 3]
+if not str.isdigit(inputNum):
+  print("정수를 입력하세요")
+elif int(inputNum) not in validInput:
+  print("1,2,3 중 하나를 입력하세요")
+  
