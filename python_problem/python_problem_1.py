5e23ba103511734db344a872d1e2e479d8ef20c7
abc2767 <73827343+abc2767@users.noreply.github.com>
2023-01-05 15:17:47 +0900

    1-5

--- python_problem/python_problem_1.py
+++ python_problem/python_problem_1.py
@@ -18,3 +18,19 @@ for i in range(inputNum):
   num += 1
   print(f"playerA: {num}")
 
+inputNum = input("부를 숫자의 개수를 입력하세요(1, 2, 3만 입력가능) :")
+
+while(True):
+  if not str.isdigit(inputNum):
+    print("정수를 입력하세요")
+    inputNum = input("부를 숫자의 개수를 입력하세요(1, 2, 3만 입력가능) :")
+  elif int(inputNum) not in validInput:
+    print("1,2,3 중 하나를 입력하세요")
+    inputNum = input("부를 숫자의 개수를 입력하세요(1, 2, 3만 입력가능) :")
+  else:
+    inputNum = int(inputNum)
+    break
+
+for i in range(inputNum):
+  num += 1
+  print(f"playerB: {num}")
